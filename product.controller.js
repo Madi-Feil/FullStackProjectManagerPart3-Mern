25ecbbae393b04ed70f4b8d67635411e33e4b3b4
Madi Greeley <98246671+Madi-Feil@users.noreply.github.com>
2022-08-13 21:44:02 -0700

    Add files via upload

---
+++ product.controller.js
@@ -0,0 +1,42 @@
+const Product = require("../models/product.model.js");
+
+module.exports = {
+    getAllProducts: (req, res) => {
+        Product.find({})
+            .then((allProducts) => res.json(allProducts))
+            .catch((err) => console.log(err));
+    },
+
+    getOneProduct: (req, res) => {
+        Product.findOne({ _id: req.params.id })
+            .then((oneProduct) => res.json(oneProduct))
+            .catch((err) => console.log(err));
+    },
+
+    createProduct: (req, res) => {
+        Product.create(req.body)
+            .then((newProduct) => res.json(newProduct))
+            .catch((err) => console.log(err));
+    },
+
+    updateProduct: (req, res) => {
+        Product.findOneAndUpdate({ _id: req.params.id}, 
+            req.body, 
+            {new:true, runValidators: true}
+            )
+            .then((updatedProduct)=>{
+                console.log(updatedProduct);
+                    res.json(updatedProduct);
+            })
+            .catch((err)=>console.log(err))
+    },
+
+    deleteProduct: (req, res)=>{
+        Product.deleteOne({_id: req.params.id})
+        .then((deletedProduct)=>{
+        console.log(deletedProduct);
+        res.json(deletedProduct);
+        })
+        .catch((err)=>console.log(err))
+    }
+};
